cf800060963f82ccc676cc535982b2de433f0b7d
Luciano Martinez <lucianomartinez27@gmail.com>
2021-04-08 19:55:03 -0300

    Escriba un script PHP que permita declarar e inicializar variables de los siguientes tipos: interger, float, string y boolean. Luego muestre en pantalla el valor de cada una de las variables antes mencionadas.

---
+++ clase_1/actividad_5_6.php
@@ -0,0 +1,12 @@
+<?php
+$numero = 0;
+
+$numeroDecimal = 1.5;
+
+$texto = 'Hola Mundo!';
+
+$verdadero = true;
+
+echo $numero.'<br>', $numeroDecimal.'<br>', $texto.'<br>', $verdadero ? 'true' : 'false';
+
+?>
