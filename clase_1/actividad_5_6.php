9cc2c4a8ccc3c1f2712fddddb0f0b241b47405e3
Luciano Martinez <lucianomartinez27@gmail.com>
2021-04-08 20:02:50 -0300

    Editar el script PHP del punto anterior y verifique que cada variable es del tipo declarado utilizando las funciones para tal caso (is_int(), is_float(), is_string(), etc.). Luego notifique por pantalla la verificación de cada una de las variables.

--- clase_1/actividad_5_6.php
+++ clase_1/actividad_5_6.php
@@ -5,8 +5,32 @@ $numeroDecimal = 1.5;
 
 $texto = 'Hola Mundo!';
 
-$verdadero = true;
 
-echo $numero.'<br>', $numeroDecimal.'<br>', $texto.'<br>', $verdadero ? 'true' : 'false';
+$verdadero = true ? 'true' : 'false';
+
+echo '<b>PARTE 1:</b><br>'.$numero.'<br>', $numeroDecimal.'<br>', $texto.'<br>', $verdadero.'<br>';
+
+
+echo '<b>PARTE 2:</b><br>';
+
+if(is_int($numero)){
+    echo $numero.'<br>';
+}
+
+if(is_float($numeroDecimal)){
+    echo $numeroDecimal.'<br>';
+}
+
+if(is_string($texto)){
+    echo $texto.'<br>';
+}
+
+if(is_bool($verdadero)){
+    echo $verdadero.'<br>';
+}
+else{
+    echo var_dump($verdadero);
+}
+
 
 ?>
