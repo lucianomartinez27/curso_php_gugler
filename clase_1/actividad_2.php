b394de81007fbc4c63159c3fdaf02cd4cc540ef0
Luciano Martinez <lucianomartinez27@gmail.com>
2021-04-08 19:41:51 -0300

    Edite el script del punto 2 y agregue un comentario de linea a cada instrucción (echo y print()) indicando la función de cada una.

--- clase_1/actividad_2.php
+++ clase_1/actividad_2.php
@@ -1,6 +1,9 @@
 <?php
 
-echo('Bienvenido al lenguaje PHP!!!<br>');
-print("PHP es un lenguaje de programación interpretado que se ejecuta del lado del servidor")
 
+// Método recomendado para incrustar código
+echo 'Bienvenido al lenguaje PHP!!!','<br>'; 
+
+// Similar a echo, pero retorna un valor y acepta sólo un argumento
+print("PHP es un lenguaje de programación\n interpretado que se ejecuta del lado del servidor"); 
 ?>
