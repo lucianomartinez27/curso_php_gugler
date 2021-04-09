19908df30cec79d2406d9eaaf6e176f60dd077a6
Luciano Martinez <lucianomartinez27@gmail.com>
2021-04-08 22:04:16 -0300

    utilice los operadores lógicos del lenguaje, realizando un comparativas del operador and (&&), or (||), xor y not (!)

---
+++ clase_1/actividad_10.php
@@ -0,0 +1,47 @@
+<?php
+
+$verdadero = true;
+
+$falso = false;
+
+// el operador & (and) necesita que ambos booleanos sean true
+
+if ($verdadero & $verdadero){
+    echo "<b>OPERADOR AND</b><br>";
+    echo "true & true == true";
+    echo "<br>";
+    echo "(true & false == false) & (false & true == false)";
+    
+}
+
+
+echo "<br>";
+
+// el operador || (or) necesita que al menos uno de los booleanos sea true
+if ($verdadero || $falso & ($verdadero || $verdadero)) {
+    echo "<b>OPERADOR OR</b><br>";
+    echo "(true || false == true) & (true || true == true)";
+    echo "<br>";
+    echo "false || false == false";
+}
+echo "<br>";
+
+// el operador ! (not) retorna la negación del booleano
+
+if (!($verdadero & $falso)){
+    echo "<b>OPERADOR NOT</b><br>";
+    echo "!true == false & !false == true";
+}
+echo "<br>";
+
+
+// el operador xor necesita que sólo uno de los operadores sea true
+
+if ($verdadero xor $ver){
+    echo "<b>OPERADOR XOR</b><br>";
+    echo "(true xor false == true) & (false xor true == true)";
+    echo "<br>";
+    echo "(false xor false == false) & (true xor true == false)";
+}
+
+?>
