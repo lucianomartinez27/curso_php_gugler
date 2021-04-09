83e9ddaf0c2110af80a71f4f00719c366748f084
Luciano Martinez <lucianomartinez27@gmail.com>
2021-04-08 21:26:18 -0300

    utilice los operadores de incremento del lenguaje, realizando un incremento de una variable y decremento de la misma.

---
+++ clase_1/actividad_9.php
@@ -0,0 +1,16 @@
+<?php
+
+$numero = 10; // declaracion de variable
+
+echo "El número comienza valiendo $numero<br>";
+
+$numero++; // operador de incremento
+
+echo "Se incrementa en uno y ahora vale $numero<br>";
+
+$numero--; // operador de decremento
+
+echo "Se decrementa en uno y ahora vale $numero, nuevamente<br>";
+
+
+?>
