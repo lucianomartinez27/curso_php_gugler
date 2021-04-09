83e550606fb95544af8e9ea72f81d39c8da656b0
Luciano Martinez <lucianomartinez27@gmail.com>
2021-04-09 10:52:44 -0300

    realice un nuevo script PHP que recorra números del 1 al 100 y que calcule el cuadrado de cada uno de ellos. Utilice la sentencia for para recorrer los mismos.

---
+++ clase_1/actividad_16.php
@@ -0,0 +1,8 @@
+<?php
+
+for ($valor = 1; $valor <= 100; $valor++){ // definimos la variable, condición e incremento
+    echo "El cuadrado de $valor es: ";
+    echo $valor ** 2; // mostramos en pantalla el cuadrado del valor de la variable
+    echo "<br>";
+} 
+?>
