80c540ed44f683c42d77e13ce3ae2cfe3f9a7cb7
Luciano Martinez <lucianomartinez27@gmail.com>
2021-04-09 09:44:46 -0300

    utilice la construcción if para determinar en que estación (verano, otoño, invierno y primavera) del año estamos de acuerdo al número de mes que se va a definir e inicializar una variable de nombre mes

---
+++ clase_1/actividad_11.php
@@ -0,0 +1,37 @@
+<?php
+
+// Disclaimer: este script no tiene en cuenta que hay meses que comparten estación.
+$mes = 1;
+
+// recorremos todos los meses posibles (de 1 al 12)
+for ($mes; $mes <=12; $mes++){
+    echo "Mes del año: $mes: <br>";
+    if ($mes >=1 & $mes < 4){ // el mes debe estar entre enero y marzo (1 a 3)
+        echo "La estación es verano";
+        echo "<br>";
+    }
+    
+    if ($mes >=4 & $mes < 7){ // el mes debe estar entre abril y junio (4 a 6)
+        echo "La estación es otoño";
+        echo "<br>";
+    }
+    
+    if ($mes >=7 & $mes < 10){ // el mes debe estar entre julio y septiembre (de 7 a 9)
+        echo "La estación es invierno";
+        echo "<br>";
+    }
+    
+    if ($mes >=10 & $mes <= 12){ // el mes debe estar entre octubre y diciembre (10 a 12)
+        echo "La estación es primavera";
+        echo "<br>";
+    }
+    echo "<br>";
+    
+    
+}
+
+
+
+
+
+?>
