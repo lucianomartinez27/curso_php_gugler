c197fbce1bc313e450319a9fdc31a68456f375d9
Luciano Martinez <lucianomartinez27@gmail.com>
2021-04-21 16:24:08 -0300

    mas manejo de string

---
+++ clase_2/anexo/actividad_6.php
@@ -0,0 +1,17 @@
+<?php
+/*Escriba un script que muestre la siguiente salida:
+La última versión estable de PHP es la 5.3.2.
+la última versión estable de php es la 5.3.2.
+LA ÚLTIMA VERSIÓN ESTABLE DE PHP ES LA 5.3.2*/
+
+$version_php = "La última versión estable de PHP es la 5.3.2.";
+
+echo $version_php;
+echo "<br>";
+echo strtolower($version_php);
+echo "<br>";
+echo strtoupper($version_php);
+
+
+
+?>
