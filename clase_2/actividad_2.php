f14119f1ac73b9b5c42c8fea77cf08e44fa6c185
Luciano Martinez <lucianomartinez27@gmail.com>
2021-04-21 14:57:31 -0300

    muestre contenido del arreglo con un foreach

--- clase_2/actividad_2.php
+++ clase_2/actividad_2.php
@@ -4,5 +4,12 @@ asignar cada uno de los días de la misma comenzando por el Domingo */
 
 $dias_semana = array('Domingo','Lunes','Martes','Miércoles','Jueves','Viernes','Sábado');
 
+ /*3) - Edite el script anterior y agregue una nueva sentencia que muestre el contenido del
+arreglo con la construcción foreach. */
+
+foreach ($dias_semana as $dia){
+    echo $dia.'<br>';
+}
+
 
 ?>
