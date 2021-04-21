a884eb094d06ff0d5463081ab963a120a08fc86d
Luciano Martinez <lucianomartinez27@gmail.com>
2021-04-21 14:56:53 -0300

    define un arreglo con los dias de semana

---
+++ clase_2/actividad_2.php
@@ -0,0 +1,8 @@
+<?php
+/*2) - Realice un script que defina un arreglo de nombre $dias_semana y al cual debemos
+asignar cada uno de los días de la misma comenzando por el Domingo */
+
+$dias_semana = array('Domingo','Lunes','Martes','Miércoles','Jueves','Viernes','Sábado');
+
+
+?>
