cdea9c774a32d7a61c47fc9c1c1e17783b388c25
Luciano Martinez <lucianomartinez27@gmail.com>
2021-05-07 16:06:20 -0300

    nueva pagina usuario y contraseña

---
+++ clase_3/parte_2/form.php
@@ -0,0 +1,11 @@
+<?php
+
+/*– Realice una nueva página Web que permita ingresar los datos de usuario y
+contraseña. Luego cree un script en PHP para que reciba los datos por POST y se muestren los
+datos. */
+
+echo $_POST['usuario'];
+echo "<br>";
+echo $_POST['contrasenia'];
+
+?>
