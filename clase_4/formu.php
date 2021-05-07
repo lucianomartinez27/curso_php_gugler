54163f2747a5cd08ae0d31449998160868faa35c
Luciano Martinez <lucianomartinez27@gmail.com>
2021-05-07 18:45:59 -0300

    función que permita validar el campo de correo electrónico

--- clase_4/formu.php
+++ clase_4/formu.php
@@ -1,18 +1,23 @@
 <?php
 
-/*Realice una página Web que contenga un formulario para cargar comentarios de una
-página Web, los campos a utilizar son los siguientes:
-Nombre
-Correo electrónico
-Página Web
-Comentario
-Luego cree un script PHP que permita obtener los datos del formulario por método POST y
-muestre los mismos por pantalla.*/
+ini_set('display_errors', 1);
+/*Edite el script PHP anterior para agregar una función que permita validar el campo de
+correo electrónico, limpiando la cadena de espacio en blancos y verificando que se encuentre el
+símbolo arroba en la cadena.*/
 
 
 foreach ($_POST as $dato => $valor){
     echo $dato.": ".$valor."<br>";
 }
+
+function validarCorreo($correo){
+    if (str_contains($correo, "@")){
+        return trim($correo);
+    }
+    throw new Exception("Correo inválido");
+}
+
+echo validarCorreo($_POST['correo']);
 
 ?>
 
