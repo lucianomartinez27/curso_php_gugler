108b2588bd4dfdbdbddbb227e2335731f06e40cf
Luciano Martinez <lucianomartinez27@gmail.com>
2021-05-07 19:25:23 -0300

    require con archivo inexistente

--- clase_4/formu.php
+++ clase_4/formu.php
@@ -2,10 +2,11 @@
 
 ini_set('display_errors', 1);
 
-/*– Crear otro script PHP que contenga la función de verificación del punto anterior. Luego
-deberá incluir (include) la misma en el script PHP del punto 1.*/
+/*– Editar el script del punto 1 para incluir el archivo pero con la sentencia require(). Luego
+pruebe indicar un nombre de archivo inexistente en la función require() para ver los resultados en
+pantalla..*/
 
-include('validar_correo.php');
+require('validar_correa.php');
 
 
 foreach ($_POST as $dato => $valor){
